480e46f2addd94355536ce6abae1659c7dd99db8
sdelamo <sergio.delamo@softamo.com>
2017-02-16 15:49:14 +0100

    Run only unit tests

--- travis-build.sh
+++ travis-build.sh
@@ -3,7 +3,7 @@ set -e
 
 export EXIT_STATUS=0
 
-./gradlew check || EXIT_STATUS=$?
+./gradlew test || EXIT_STATUS=$?
 
 if [[ $EXIT_STATUS -eq 0 && $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
     echo "Publishing to PWS" 
