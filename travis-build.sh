f630792edfd5e8e80404012c902647582d551dd3
sdelamo <sergio.delamo@softamo.com>
2017-02-16 17:15:37 +0100

    Remove logs statements from travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -8,13 +8,17 @@ export EXIT_STATUS=0
 echo "Tag: $TRAVIS_TAG"
 
 if [[ $EXIT_STATUS ]]; then
+
     if [[ ( $TRAVIS_BRANCH == master || $TRAVIS_TAG == prod_* ) && $TRAVIS_PULL_REQUEST == 'false' ]]; then
-       echo "This will deploy!"
-       echo "Publishing to PWS" 
-       ./grailsw war || EXIT_STATUS=$?
-       ./gradlew -PcfUsername=$CF_USERNAME -PcfPassword=$CF_PASSWORD cfPush || EXIT_STATUS=$i
-    else
-       echo "This will not deploy!"
+
+        echo "Publishing to PWS" 
+
+        ./grailsw war || EXIT_STATUS=$?
+
+        if [[ $EXIT_STATUS ]]; then
+            ./gradlew -PcfUsername=$CF_USERNAME -PcfPassword=$CF_PASSWORD cfPush || EXIT_STATUS=$i
+        fi
+
     fi
 fi
 exit $EXIT_STATUS
