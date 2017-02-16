1f6bc3870d47bfa8130b50922d8857305eff1a67
sdelamo <sergio.delamo@softamo.com>
2017-02-16 15:40:08 +0100

    Fix travis configuration

--- travis-build.sh
+++ travis-build.sh
@@ -7,6 +7,7 @@ export EXIT_STATUS=0
 
 if [[ $EXIT_STATUS -eq 0 && $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
     echo "Publishing to PWS" 
+    ./gradlew -PcfUsername=$CF_USERNAME -PcfPassword=$CF_PASSWORD cfPush
 fi
 
 exit $EXIT_STATUS
