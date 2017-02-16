bbfc65c1dc39c9024076488bbf078445c71aa46b
sdelamo <sergio.delamo@softamo.com>
2017-02-16 17:06:44 +0100

    fix travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -7,12 +7,14 @@ export EXIT_STATUS=0
 
 echo "Tag: $TRAVIS_TAG"
 
-if [[ $EXIT_STATUS -eq 0 && -n $TRAVIS_TAG && $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
-    echo "Publishing to PWS" 
-    
-    ./grailsw war || EXIT_STATUS=$?
-
-    ./gradlew -PcfUsername=$CF_USERNAME -PcfPassword=$CF_PASSWORD cfPush || EXIT_STATUS=$i
+if [[ $EXIT_STATUS ]]; then
+    if [[ ( $TRAVIS_BRANCH == master || $TRAVIS_TAG == prod_* ) && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+       echo "This will deploy!"
+       echo "Publishing to PWS" 
+       ./grailsw war || EXIT_STATUS=$?
+       ./gradlew -PcfUsername=$CF_USERNAME -PcfPassword=$CF_PASSWORD cfPush || EXIT_STATUS=$i
+    else
+       echo "This will not deploy!"
+    fi
 fi
-
 exit $EXIT_STATUS
