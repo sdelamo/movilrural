3d8e5deccb102805603c4c21522c609a504cef57
sdelamo <sergio.delamo@softamo.com>
2017-02-16 16:45:54 +0100

    Deploy if tagged

--- travis-build.sh
+++ travis-build.sh
@@ -5,8 +5,11 @@ export EXIT_STATUS=0
 
 ./gradlew test || EXIT_STATUS=$?
 
-if [[ $EXIT_STATUS -eq 0 && $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+if [[ $EXIT_STATUS -eq 0 ]] && [[ -n $TRAVIS_TAG ]] && [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
     echo "Publishing to PWS" 
+    
+    ./grailsw war
+
     ./gradlew -PcfUsername=$CF_USERNAME -PcfPassword=$CF_PASSWORD cfPush
 fi
 
