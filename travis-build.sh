a4246815406bf0a17786a4b1434fa2e147677cf4
sdelamo <sergio.delamo@softamo.com>
2017-02-20 11:50:04 +0100

    Fix travis.build

--- travis-build.sh
+++ travis-build.sh
@@ -6,28 +6,19 @@ export EXIT_STATUS=0
 ./gradlew test || EXIT_STATUS=$?
 
 echo "Tag: $TRAVIS_TAG"
-
-if [[ $TRAVIS_BRANCH != master ]]; then
-    exit 0
-fi
-if [[ $TRAVIS_PULL_REQUEST == 'true' ]]; then
-    exit 0
-fi
+exit 0fi
 
 if [[ $EXIT_STATUS ]]; then
 
-    echo "Checking if tag set"
+    if [[ -n $TRAVIS_TAG ]] && [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
 
-    if [[-n "$TRAVIS_TAG"]]; then
-
-        echo "Publishing to PWS" 
+        echo "Publishing to PWS"
 
         ./grailsw war || EXIT_STATUS=$?
 
         if [[ $EXIT_STATUS ]]; then
             ./gradlew -PcfUsername=$CF_USERNAME -PcfPassword=$CF_PASSWORD cfPush || EXIT_STATUS=$i
         fi
-
     fi
 fi
 exit $EXIT_STATUS
